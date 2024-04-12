e904bb077f612b528e0db228e5be2d0d2055b592
y-kamiya <y.kamiya0@gmail.com>
2024-04-12 20:19:38 +0900

    prompt2prompt

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 pip install torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2
-pip install transformers accelerate
+pip install transformers accelerate matplotlib opencv-python
 pip install -e .
