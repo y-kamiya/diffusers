81b42479d8ea07a6c6cb2b723d83a7094c78b965
y-kamiya <y.kamiya0@gmail.com>
2024-04-16 19:42:56 +0900

    remove cache

--- experiments/nullinversion/pipeline.py
+++ experiments/nullinversion/pipeline.py
@@ -182,12 +182,9 @@ class NullTextPipeline(StableDiffusionPipeline):
         context = self.get_context(prompt)
         latent = self.image2latent(image_path)
         ddim_latents = self.ddim_inversion_loop(latent, context)
-        if os.path.exists(image_path + ".pt"):
-            uncond_embeddings = torch.load(image_path + ".pt")
-        else:
-            uncond_embeddings = self.null_optimization(ddim_latents, context, num_inner_steps, early_stop_epsilon)
-            uncond_embeddings = torch.stack(uncond_embeddings, 0)
-            torch.save(uncond_embeddings, image_path + ".pt")
+        uncond_embeddings = self.null_optimization(ddim_latents, context, num_inner_steps, early_stop_epsilon)
+        uncond_embeddings = torch.stack(uncond_embeddings, 0)
+        torch.save(uncond_embeddings, image_path + ".pt")
         return ddim_latents[-1], uncond_embeddings
 
     @torch.no_grad()
