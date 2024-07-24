c854327cb779f6ad6a7eb0e283c79d201e545365
y-kamiya <y.kamiya0@gmail.com>
2024-07-24 11:29:44 +0900

    use latest pytorch

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pip install torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2
+pip install torch torchvision torchaudio
 pip install transformers accelerate matplotlib opencv-python
 pip install -e .
